2085cbf19fbaa2a623ad688ace8678779c2c503e
danielmuschler <dmusch4@gmail.com>
2018-10-15 17:12:01 -0500

    Add all products and one product actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,12 @@
 class Api::ProductsController < ApplicationController
-  def product_model
+  def all_products_method
+    @products = Product.all
     render "products.json.jbuilder"
+  end
+
+  def one_product_method
+    @product = Product.find_by(id: params["product_id"])
+    render "one_product.json.jbuilder"
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
-    get "/products_url" => "products#product_model"
+    get "/all_products_url" => "products#all_products_method"
+    get "/one_product_url" => "products#one_product_method"
+    get "/one_product_url/:product_id" => "products#one_product_method"
   end
 end
